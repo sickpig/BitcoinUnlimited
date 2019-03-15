82ab5ecb30c3977920dc8f9e0643719cd9c3837c
Andrea Suisani <sickpig@gmail.com>
2019-03-15 17:44:54 +0100

    Move fork helpers from uahf_fork.cpp into validation/forks.cpp

--- src/validation/forks.cpp
+++ src/validation/forks.cpp
@@ -3,7 +3,77 @@
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
 #include "forks.h"
+#include "chain.h"
+#include "chainparams.h"
+#include "primitives/block.h"
+#include "script/interpreter.h"
+#include "txmempool.h"
 #include "unlimited.h"
+
+#include <inttypes.h>
+#include <vector>
+
+bool IsTxProbablyNewSigHash(const CTransaction &tx)
+{
+    bool oldsighash = false;
+    for (auto txin : tx.vin)
+    {
+        std::vector<unsigned char> data;
+        CScript::const_iterator pc(txin.scriptSig.begin());
+        opcodetype op;
+        if (txin.scriptSig.GetOp(pc, op, data))
+        {
+            if (!data.empty())
+            {
+                if
+                    !(data.back() & SIGHASH_FORKID) { oldsighash = true; }
+            }
+        }
+    }
+    return (oldsighash == false);
+}
+
+bool IsTxUAHFOnly(const CTxMemPoolEntry &txentry)
+{
+    if ((txentry.sighashType & SIGHASH_FORKID) || (txentry.sighashType == 0))
+    {
+        return true;
+    }
+    return false;
+}
+
+// return true for every block from fork block and forward [consensusParams.uahfHeight,+inf)
+bool UAHFforkActivated(int height)
+{
+    const Consensus::Params &consensusParams = Params().GetConsensus();
+    if (height >= consensusParams.uahfHeight)
+    {
+        return true;
+    }
+    return false;
+}
+
+// This will check if the Fork will be enabled at the next block
+// i.e. we are at block x - 1, [x-1, +inf]
+// state fork: enabled or activated
+bool IsUAHFforkActiveOnNextBlock(int height)
+{
+    const Consensus::Params &consensusParams = Params().GetConsensus();
+    if (height >= (consensusParams.uahfHeight - 1))
+        return true;
+    return false;
+}
+
+// return true only if 1st condition is true (Median past time > UAHF time)
+// and not the 2nd, i.e. we are at precisely [x-1,x-1]
+// state: fork enabled
+bool UAHFforkAtNextBlock(int height)
+{
+    const Consensus::Params &consensusParams = Params().GetConsensus();
+    if (height == (consensusParams.uahfHeight - 1))
+        return true;
+    return false;
+}
 
 // For pindexTip use the current chainActive.Tip().
 
--- src/validation/forks.h
+++ src/validation/forks.h
@@ -5,8 +5,37 @@
 #ifndef BITCOIN_FORKS_H
 #define BITCOIN_FORKS_H
 
+#include "amount.h"
+#include "arith_uint256.h"
 #include "chain.h"
 #include "consensus/params.h"
+#include "tweak.h"
+#include "univalue/include/univalue.h"
+#include <vector>
+
+class CValidationState;
+class CBlock;
+class CTransaction;
+class CBlockIndex;
+class CScript;
+class CTxMemPoolEntry;
+
+// Return true if this transaction can only be committed post Aug, 1st 2017 fork
+extern bool IsTxUAHFOnly(const CTxMemPoolEntry &tx);
+
+// It is not possible to provably determine whether an arbitrary script signs using the old or new sighash type
+// without executing the previous output and input scripts.  But we can make a good guess by assuming that
+// these are standard scripts.
+bool IsTxProbablyNewSigHash(const CTransaction &tx);
+
+// was the fork activated on this or any prior block?
+extern bool UAHFforkActivated(int height);
+
+// Is the next block the fork block?
+extern bool UAHFforkAtNextBlock(int height);
+
+// Is the fork active on the next block?
+extern bool IsUAHFforkActiveOnNextBlock(int height);
 
 /** Check is Cash HF has activated. */
 bool IsDAAEnabled(const Consensus::Params &consensusparams, const CBlockIndex *pindexTip);
