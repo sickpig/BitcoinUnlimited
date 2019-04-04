e30d533f7cac03ac812cfe45dba8431e27b79ba2
Andrea Suisani <sickpig@gmail.com>
2019-04-04 17:18:21 +0200

    Switch to DEFAULT_EXCESSIVE_BLOCK_SIZE only on BCH main chain.

--- src/validation/validation.cpp
+++ src/validation/validation.cpp
@@ -2748,7 +2748,10 @@ void UpdateTip(CBlockIndex *pindexNew)
     else
     {
         maxScriptOps = MAX_OPS_PER_SCRIPT;
-        excessiveBlockSize = DEFAULT_EXCESSIVE_BLOCK_SIZE;
+        if (chainParams.NetworkIDString() != "regtest")
+        {
+            excessiveBlockSize = DEFAULT_EXCESSIVE_BLOCK_SIZE;
+        }
     }
 }
 
